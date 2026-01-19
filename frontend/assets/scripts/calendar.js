f8fee39b8b0916ea0ca21184c0bc878761eaf99b
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2026-01-18 22:23:38 -0400

    feat: Implemented functionality for calendar dates

---
+++ frontend/assets/scripts/calendar.js
@@ -0,0 +1,95 @@
+import DateHelper from "./dateHelper.js"
+
+// CONSTANTS
+const weekdays = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+]
+
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+]
+
+const offmonthDayClass = "calendar-date-offmonth"
+const weekdayClass = "calendar-weekday"
+const dateClass = "calendar-date"
+// END CONSTANTS
+
+// Elements
+const currentDate = new DateHelper()
+const currentMonthDiv = document.getElementById("current-month-div")
+const calendarDaysGrid = document.getElementById("calendar-days-grid")
+const calendarLeftButton = document.getElementById("calendar-left-button")
+const calendarRightButton = document.getElementById("calendar-right-button")
+
+calendarLeftButton.addEventListener("click", (e) => {
+	if (e.button !== 0) return
+	currentDate.addMonth(-1)
+	currentDate.setDate(1)
+	RebuildCalendar()
+})
+
+calendarRightButton.addEventListener("click", (e) => {
+	if (e.button !== 0) return
+	currentDate.addMonth(1)
+	currentDate.setDate(1)
+	RebuildCalendar()
+})
+
+function OnWindowLoad() {
+	RebuildCalendar()
+}
+
+function RebuildCalendar() {
+	calendarDaysGrid.innerHTML = ""
+
+	currentMonthDiv.innerHTML = `${months[currentDate.getMonth()]} ${currentDate.getFullYear()}`
+	weekdays.forEach((day) => {
+		const dayDiv = document.createElement("div")
+		dayDiv.classList.add(weekdayClass)
+		dayDiv.innerHTML = day
+		calendarDaysGrid.appendChild(dayDiv)
+	})
+
+	let currentMonth = false
+	let daysInCurrentMonth = currentDate.getDaysInPreviousMonth(currentDate)
+	const firstWeekdayOfMonth = currentDate.getFirstWeekdayOfMonth(currentDate)
+	let dayNumber =
+		daysInCurrentMonth -
+		(firstWeekdayOfMonth === 0 ? 7 : firstWeekdayOfMonth) +
+		1
+	for (let i = 0; i < weekdays.length; i++) {
+		for (let j = 0; j < 6; j++) {
+			const dayDiv = document.createElement("div")
+			dayDiv.innerHTML = dayNumber
+			dayDiv.classList.add(dateClass)
+			if (!currentMonth) dayDiv.classList.add(offmonthDayClass)
+			calendarDaysGrid.appendChild(dayDiv)
+			dayNumber = (dayNumber + 1) % (daysInCurrentMonth + 1)
+			if (dayNumber === 0) {
+				dayNumber++
+				daysInCurrentMonth = currentDate.getDaysInCurrentMonth(currentDate)
+				currentMonth = !currentMonth
+			}
+		}
+	}
+}
+
+// Setup
+window.onload = () => OnWindowLoad()
